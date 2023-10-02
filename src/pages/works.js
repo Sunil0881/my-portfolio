4a6c2a536903ed20b8e7184e3d7dee7eb567b843
Sunil_0881 <sunil12162003@gmail.com>
2023-10-02 14:57:51 +0530

    fc

--- src/pages/works.js
+++ src/pages/works.js
@@ -1,5 +1,7 @@
 
 import guitar from '../assets/guitar.png'
+import pfolio from '../assets/pfolio.png'
+import fend from '../assets/fend.png'
 
 const Works = () => {
     return (
@@ -9,10 +11,13 @@ const Works = () => {
                     <p className="font-bold text-5xl text-white pl-48 ">My  <span className="text-red-500">Works</span></p>
                     <p className="text-white pl-48 pt-3 pb-44">Explore my works to know more about my skills</p>
                 </div>
-
+                
+                <a href="https://portfolio-by-gknights.netlify.app/" className="pl-3">
                 <div className="bg-white/10  text-white  w-[250px] h-[200px] rounded-3xl ">
-                    <img src={guitar} alt="guitar"></img>
-                </div>
+                    <img src={pfolio} alt="projectimg"></img>
+                    <p className="font-bold text-white  pt-3 pl-3">Classic Portfolio Design</p>
+                <a href="https://portfolio-by-gknights.netlify.app/" className="pl-3">Live Project</a>
+                </div></a>
 
                 <a href="https://onlineguitar-by-gknights.netlify.app/">
                 <div className="bg-white/10   text-white  w-[250px] h-[200px] rounded-3xl ">  
@@ -23,8 +28,12 @@ const Works = () => {
 
 
               
-                <div className="bg-white/10  text-white  w-[250px] h-[200px] rounded-3xl ">
-                <img src={guitar} alt="guitar"></img>  </div>
+                <a href="https://animated-frontend-design-by-gknights.netlify.app/">
+                <div className="bg-white/10   text-white  w-[250px] h-[200px] rounded-3xl ">  
+                <img src={fend} alt="projectimg"></img>
+                <p className="font-bold text-white  pt-3 pl-3">Animated Frontend Model</p>
+                <a href="https://animated-frontend-design-by-gknights.netlify.app/" className="pl-3">Live Project</a>
+                </div></a>
 
 
               
