912052a27d76941c338643688a9e107b626ad5bc
Sunil_0881 <sunil12162003@gmail.com>
2023-10-02 21:32:45 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, } from 'react-router-dom'
 import Home from './pages/home';
 import Works from './pages/works';
 import Contact from './pages/contact';
